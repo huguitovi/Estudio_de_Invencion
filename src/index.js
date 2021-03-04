232b698f092dcf1716b9f5e44d6675c37b1e3458
huguitovi <vhastorga@gmail.com>
2021-03-04 01:31:08 -0300

    broken img

--- src/index.js
+++ src/index.js
@@ -6,37 +6,35 @@ import './index.css'
 
 const firstBook = {
   title: 'Hello World',
-  imag:
+  img:
     'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
   author: 'victor',
 }
 
 const secBook = {
   title: 'Other World',
-  imag:
+  img:
     'https://marketing4ecommerce.net/wp-content/uploads/2018/01/Depositphotos_3667865_m-2015-compressor.jpg',
   author: 'hugo',
 }
 const tirstBook = {
   title: 'Hi Moon!',
-  imag:
+  img:
     'https://image.freepik.com/vector-gratis/vector-onda-azul-transparente_1055-7084.jpg',
   author: 'Ezequiel',
 }
 
 const gecBook = {
   title: 'Other Moon',
-  imag: 'https://www.idento.es/wp-content/uploads/2015/05/Png-vs-Jpg-1.jpg',
+  img: 'https://www.idento.es/wp-content/uploads/2015/05/Png-vs-Jpg-1.jpg',
   author: 'Fernando',
 }
-const Book = (props) => {
+const Book = ({ img, title, author }) => {
   return (
     <article className='bo'>
-      <img width={'200rem'} height={'150rem'} src={props.imag} alt='' />
-      <h1>{props.title}</h1>
-      <h2>{props.author}</h2>
-      <p>{props.job}</p>
-      <p>{props.number}</p>
+      <img width={'200rem'} height={'150rem'} src={img} alt='' class='center' />
+      <h2>{title}</h2>
+      <h1>{author}</h1>
     </article>
   )
 }
@@ -45,48 +43,67 @@ const ListBook = () => {
     <section className='boolist'>
       <Book
         author={firstBook.author}
-        imag={firstBook.imag}
+        imag={firstBook.img}
         title={firstBook.title}
         number={6 + 3}
-      />{' '}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam
+          lobortis facilisis sem. Nullam nec mi et neque pharetrasollicitudin.
+          Praesent imperdiet mi nec ante. Donec ullamcorper, felis non sodales
+          commodo, lectus velit ultrices augue, adignissim nibh lectus placerat
+          pede. Vivamus nunc nunc, molestie ut, ultricies vel, semper in, velit.
+          Ut porttitor. Praesentin sapien. Lorem ipsum dolor sit amet,
+          consectetuer adipiscing elit. Duis fringilla tristique neque. Sed
+          interdum libero utmetus. Pellentesque placerat. Nam rutrum augue a
+          leo. Morbi sed elit sit amet ante lobortis sollicitudin.
+        </p>
+      </Book>{' '}
       <Book
-        imag={secBook.imag}
+        imag={secBook.img}
         title={secBook.title}
         author={secBook.author}
         number={3}
-      />{' '}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+          vitae totam praesentium, nisi sed laborum reiciendis placeat ratione,
+          soluta maiores amet nesciunt sapiente error tenetur corporis impedit
+          minima? Magni, explicabo!
+        </p>
+      </Book>
       <Book
-        imag={gecBook.imag}
+        imag={gecBook.img}
         title={gecBook.title}
         author={gecBook.author}
         number={3}
       />{' '}
       <Book
-        imag={tirstBook.imag}
+        imag={tirstBook.img}
         title={tirstBook.title}
         author={tirstBook.author}
         number={3}
       />{' '}
       <Book
         author={firstBook.author}
-        imag={firstBook.imag}
+        imag={firstBook.img}
         title={firstBook.title}
         number={6 + 3}
       />{' '}
       <Book
-        imag={secBook.imag}
+        imag={secBook.img}
         title={secBook.title}
         author={secBook.author}
         number={3}
       />{' '}
       <Book
-        imag={gecBook.imag}
+        imag={gecBook.img}
         title={gecBook.title}
         author={gecBook.author}
         number={3}
       />{' '}
       <Book
-        imag={tirstBook.imag}
+        imag={tirstBook.img}
         title={tirstBook.title}
         author={tirstBook.author}
         number={3}
