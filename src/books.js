78cc49701d1c831eb65343f27c4438aa12ff27a9
huguitovi <vhastorga@gmail.com>
2021-03-04 02:35:23 -0300

    clicks & buttons

---
+++ src/books.js
@@ -0,0 +1,58 @@
+export const books = [
+  {
+    id: 1,
+    title: 'Hello World',
+    img:
+      'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
+    author: 'victor',
+  },
+
+  {
+    id: 2,
+    title: 'Other World',
+    img:
+      'https://marketing4ecommerce.net/wp-content/uploads/2018/01/Depositphotos_3667865_m-2015-compressor.jpg',
+    author: 'hugo',
+  },
+  {
+    id: 3,
+    title: 'Hi Moon!',
+    img:
+      'https://image.freepik.com/vector-gratis/vector-onda-azul-transparente_1055-7084.jpg',
+    author: 'Ezequiel',
+  },
+
+  {
+    id: 4,
+    title: 'Other Moon',
+    img: 'https://www.idento.es/wp-content/uploads/2015/05/Png-vs-Jpg-1.jpg',
+    author: 'Fernando',
+  },
+  {
+    title: 'Hello World',
+    img:
+      'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
+    author: 'victor',
+  },
+
+  {
+    title: 'Other World',
+    img:
+      'https://marketing4ecommerce.net/wp-content/uploads/2018/01/Depositphotos_3667865_m-2015-compressor.jpg',
+    author: 'hugo',
+  },
+  {
+    title: 'Hi Moon!',
+    img:
+      'https://image.freepik.com/vector-gratis/vector-onda-azul-transparente_1055-7084.jpg',
+    author: 'Ezequiel',
+    massage:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero nostrum in, nemo vitae perferendis quia possimus culpa nesciunt alias, illum maxime ullam exercitationem odio quas unde modi quos est? Officiis.',
+  },
+
+  {
+    title: 'Other Moon',
+    img: 'https://www.idento.es/wp-content/uploads/2015/05/Png-vs-Jpg-1.jpg',
+    author: 'Fernando',
+  },
+]
